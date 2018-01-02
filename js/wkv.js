7a3ec9397ae688817f3abcdef0ceb94b56a45e60
John Leonard <jjleonard@gmail.com>
2018-01-02 07:55:50 +0000

    updated with useful info in comments

--- js/wkv.js
+++ js/wkv.js
@@ -34,4 +34,7 @@ TODO:
 * work out how to return the leading test from the article (first sentence, not snippet)
 * then add a search box to the page
 * pass the search value to the query after rudimentary checks
+* HELPFUL INFO:
+* link to ssample search (note list=search and srsearch holding query): /w/api.php?action=query&format=json&prop=extracts&list=search&srsearch=battle%20of%20britain
+* API Sandbox link showing parameters: https://en.wikipedia.org/wiki/Special:ApiSandbox#action=query&format=json&prop=extracts&list=search&srsearch=battle%20of%20britain
 */
