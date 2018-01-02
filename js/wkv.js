a642f303a4f1c5329195fabe172957d6341516bd
John Leonard <jjleonard@gmail.com>
2018-01-02 20:32:20 +0000

    fix API query, list results, link to wiki pages

--- js/wkv.js
+++ js/wkv.js
@@ -1,6 +1,6 @@
 var endpoint="https://en.wikipedia.org/w/api.php?";
-/* var test="action=query&list=random&rnlimit=1&rnnamespace=0&format=json"; */
-var test="action=opensearch&search=Apple&limit=10&namespace=0&format=json";
+var wkTitleLinkPre="https://en.wikipedia.org/wiki/";
+var test="action=query&format=json&prop=extracts&list=search&srsearch=battle%20of%20britain";
 
 $(document).ready(function(){
   $("#random").on("click", function(){
@@ -22,18 +22,24 @@ $(document).ready(function(){
 });
 
 function display(data){
-  alert(JSON.stringify(data));
-  var tempVar = data[0][0];
-  alert(tempVar);
-  $("#wkPageTitle").html(tempVar);
-  // alert(data.query.pages["15580374"].title);
+  var numResults = data["continue"]["sroffset"];
+  for(i=0;i<numResults;i++){
+    var wkEntryTitle = data["query"]["search"][i]["title"];
+    var wkEntrySnippet = data["query"]["search"][i]["snippet"];
+    $("ul").append('<li><a href="'+wkTitleLinkPre+wkEntryTitle+'">'+wkEntryTitle+'</a></li>'+'<p>'+wkEntrySnippet+'</p>');
+  };
 };
 
 /*
 TODO:
-* work out how to return the leading test from the article (first sentence, not snippet)
-* then add a search box to the page
-* pass the search value to the query after rudimentary checks
+* work out how to return the leading test from the article (first sentence, not snippet) - DONE (also displayed the content as a series of entries)
+* then add a search box to the page - DONE
+* pass the search value to the query after rudimentary checks - I have to work out how to pull the text from the input when
+  the search button is clicked.
+
+AFTER THAT:
+* clean up, tidy up
+
 * HELPFUL INFO:
 * link to ssample search (note list=search and srsearch holding query): /w/api.php?action=query&format=json&prop=extracts&list=search&srsearch=battle%20of%20britain
 * API Sandbox link showing parameters: https://en.wikipedia.org/wiki/Special:ApiSandbox#action=query&format=json&prop=extracts&list=search&srsearch=battle%20of%20britain
