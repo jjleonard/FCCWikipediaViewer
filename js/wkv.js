70eabc954fa2195c35e0497d06ea3468b6e1788d
John Leonard <jjleonard@gmail.com>
2018-01-03 21:33:02 +0000

    enable search function, add bootstrap, clean up functions

--- js/wkv.js
+++ js/wkv.js
@@ -1,23 +1,10 @@
 var endpoint="https://en.wikipedia.org/w/api.php?";
 var wkTitleLinkPre="https://en.wikipedia.org/wiki/";
-var test="action=query&format=json&prop=extracts&list=search&srsearch=battle%20of%20britain";
 
 $(document).ready(function(){
-  $("#random").on("click", function(){
-    $.ajax({
-      url: endpoint+test,
-      data: {
-        format: 'json'
-      },
-      type: 'GET',
-      // Api-User_agent below is recommended when
-      // querying so they can identify you - format is free-form
-      headers: {
-        'Api-User-Agent': 'jjleonard@gmail.com'
-      },
-      dataType: 'jsonp',
-      jsonpCallback: "display",
-    });
+  $("form").submit(function(e){
+    e.preventDefault();
+    searchWiki(endpoint+"action=query&format=json&prop=extracts&list=search&srsearch="+$("#search").val());
   });
 });
 
@@ -26,19 +13,40 @@ function display(data){
   for(i=0;i<numResults;i++){
     var wkEntryTitle = data["query"]["search"][i]["title"];
     var wkEntrySnippet = data["query"]["search"][i]["snippet"];
-    $("ul").append('<li><a href="'+wkTitleLinkPre+wkEntryTitle+'">'+wkEntryTitle+'</a></li>'+'<p>'+wkEntrySnippet+'</p>');
+    $(".list-group").append('<a class=list-group-item list-group-item-action" href="'+wkTitleLinkPre+wkEntryTitle+'"><strong>'+wkEntryTitle+'</strong>'+'<p>'+wkEntrySnippet+'</p></a>');
   };
 };
+
+function searchWiki(searchUrl){
+  $.ajax({
+    url: searchUrl,
+    data: {
+      format: 'json'
+    },
+    type: 'GET',
+    // Api-User_agent below is recommended when
+    // querying so they can identify you - format is free-form
+    headers: {
+      'Api-User-Agent': 'jjleonard@gmail.com'
+    },
+    dataType: 'jsonp',
+    jsonpCallback: "display",
+  });
+};
+
+
 
 /*
 TODO:
 * work out how to return the leading test from the article (first sentence, not snippet) - DONE (also displayed the content as a series of entries)
 * then add a search box to the page - DONE
 * pass the search value to the query after rudimentary checks - I have to work out how to pull the text from the input when
-  the search button is clicked.
+  the search button is clicked. - DONE
+* work out how to clear the search bar and re-enable the button as it only works once between page refreshes
+* re-apply the random button function
 
 AFTER THAT:
-* clean up, tidy up
+* clean up, tidy up - some bootstrap needed, need to re-arrange the page layout to make it prettier
 
 * HELPFUL INFO:
 * link to ssample search (note list=search and srsearch holding query): /w/api.php?action=query&format=json&prop=extracts&list=search&srsearch=battle%20of%20britain
